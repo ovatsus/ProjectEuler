668d8415d9241c0679ffbc7ab491b0598f6bff15
Gustavo Guerra <gustavo@codebeside.org>
2011-12-14 00:06:58 +0000

    Problems 1, 2 & 3

---
+++ 1.fsx
@@ -0,0 +1,7 @@
+﻿
+/// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+/// Find the sum of all the multiples of 3 or 5 below 1000.
+let pe1() = // result: 233168
+    seq { 1..999 } 
+    |> Seq.filter(fun x -> x % 5 = 0 || x % 3 = 0)
+    |> Seq.sum
---
+++ 3.fsx
@@ -0,0 +1,23 @@
+﻿
+/// The prime factors of 13195 are 5, 7, 13 and 29.
+/// What is the largest prime factor of the number 600851475143 ?
+let pe3() = // result: 6857
+    let isPrime x = 
+        { 2L .. int64(sqrt(float x)) } 
+        |> Seq.forall (fun i -> x % i <> 0L) 
+    let allPrimes = 
+        Seq.initInfinite int64 
+        |> Seq.skip 2
+        |> Seq.filter isPrime
+        |> Seq.cache
+    let rec factors x primes = seq {
+        if x <> 1L then
+            let factor = Seq.head primes
+            if x % factor = 0L then
+                yield factor
+                yield! factors (x / factor) (Seq.skip 1 primes)
+            else
+                yield! factors x (Seq.skip 1 primes)
+    }
+    factors 600851475143L allPrimes
+    |> System.Linq.Enumerable.Last
